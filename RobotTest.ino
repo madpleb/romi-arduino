f4e3115e09b8f7715e697679a7b6f33ce6327171
Jerry <meebdraws@gmail.com>
2026-02-14 11:16:07 -0800

    Add files via upload

--- RobotTest.ino
+++ RobotTest.ino
@@ -1,3 +1,19 @@
-int main() {
-  return 0;
+
+#include "Robot.h"
+
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(lPin, OUTPUT);
+  pinMode(rPin, OUTPUT);
+  // Play startup sound.
+  calibrate(freq);
+
+
+}
+
+void loop() {
+  int arr[] {50, 25, 2, 2, 0};
+  startRobot(arr);
 }
